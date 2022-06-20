62de2c3e66327481866e2e88a062c4e2d10d98ab
abolfazl-MI <abolfazlmashhadi837@gmail.com>
2022-06-20 12:22:31 -0500

    home Screen impelented

---
+++ lib/HomeScreen/views/home_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Column(
+        children: [
+          _appBar(width: width, height: height),
+          _body(width: width, height: height)
+        ],
+      ),
+    );
+  }
+
+  _appBar({double? width, double? height}) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      width: width,
+      height: height! * 0.2,
+      // color: Colors.red,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.menu_outlined,
+              size: 40,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal:20),
+            child: const Text(
+              'Lists',
+              style: TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+                letterSpacing:0.1
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+  _body({double ? width, double ?  height}){
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      width: width,
+      height: height!*0.8,
+      // color: Colors.green,
+      child: GridView.builder(gridDelegate:const  SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 5,
+        crossAxisSpacing: 5
+      ), itemBuilder: (context,index){
+        return Card(
+
+          elevation: 12,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                FlutterLogo(size: 50),
+               Padding(
+                 padding:  EdgeInsets.all(20.0),
+                 child:   Text('Work', style: TextStyle(
+                    fontSize: 30,
+                    // fontWeight: FontWeight.bold
+                  ),),
+               ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text('61Task', style: TextStyle(
+                    // fontSize: 30,
+                    fontWeight: FontWeight.w400
+                  )),
+                )
+              ],
+            ),
+          ),
+        );
+      }),
+    );
+  }
+}
