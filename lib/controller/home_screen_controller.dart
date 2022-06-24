78e072fb4b0c020776de92500cafe9b18ed62147
Abolfazl-MI <abolfazlmashhadi837@gmail.com>
2022-06-24 11:34:47 -0400

    hive services has been created

--- lib/controller/home_screen_controller.dart
+++ lib/controller/home_screen_controller.dart
@@ -1,8 +1,5 @@
 import 'package:get/state_manager.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:hive_todo_app/models/proeject_task_modle.dart';
-import 'package:hive_todo_app/models/project_model.dart';
-import 'package:hive_todo_app/models/project_status_model.dart';
+
 
 class HomeController extends GetxController {
   @override
