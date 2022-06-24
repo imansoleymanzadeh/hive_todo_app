9dd1593e7770afe24b14cfe540efefcf2109e433
Aminarefrad <coderad21@gmail.com>
2022-06-24 12:49:42 +0430

    create detailScreen

--- lib/views/detaile_screen.dart
+++ lib/views/detaile_screen.dart
@@ -9,6 +9,194 @@ class DetaileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Stack(
+          children:[ 
+            //appbar Todo 
+            Appbartodo(size: size),
+
+            //body_todo
+           BodyTodo(size: size)
+
+        ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child:const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class Appbartodo extends StatelessWidget {
+  const Appbartodo({
+    Key? key,
+    required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: size.height*0.5,
+      color: Colors.blue,
+      child: SafeArea(
+        child:Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+    margin:const EdgeInsets.symmetric(horizontal: 10),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            size: 30,
+            color: Colors.white,
+          ),
+        ),
+        
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.more_vert_outlined,
+            size: 30,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    ),
+          ),
+          Container(
+    margin:const EdgeInsets.only(left: 40.0, top: 120.0),
+    child:const CircleAvatar(
+      radius: 35,
+      backgroundColor: Colors.white,
+      child: Center(
+        child: Icon(
+          
+          Icons.paste_rounded,
+          color: Colors.blue,
+          size: 30,
+        ),
+      ),
+    ),
+          ),
+          Container(
+    margin:const EdgeInsets.only(left: 48.0, top: 12.0),
+    child:const Text(
+      "All",
+      style: TextStyle(
+          color: Colors.white,
+          fontSize: 30.0,
+          fontWeight: FontWeight.bold),
+    ),
+          ),
+          Container(
+      margin:const EdgeInsets.only(
+        left: 47.0,
+      ),
+      child:const  
+         Text(
+          "0 Tasks",
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+              fontWeight: FontWeight.w500),
+        )
+      ),
+        ],
+      )  ,
+      ),
+    );
+  }
+}
+
+class BodyTodo extends StatelessWidget {
+  const BodyTodo({
+    Key? key,
+    required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+     width: double.infinity,
+    margin:EdgeInsets.only(top: size.height*0.45) ,
+    decoration:const BoxDecoration(
+     color: Colors.white,
+     borderRadius: BorderRadius.only(
+       topLeft: Radius.circular(30),
+       topRight: Radius.circular(30)
+     )
+    ),
+    child: Padding(
+      padding: const EdgeInsets.fromLTRB(16,20,20,0),
+      child: Column(
+       crossAxisAlignment: CrossAxisAlignment.start ,
+       children: [
+       const  Text("Late",style: TextStyle(fontSize: 17),),
+       const  SizedBox(height: 10,),
+         Container(
+             width: double.infinity,
+             height: size.height/12.2,
+               decoration: BoxDecoration(
+              
+               borderRadius: BorderRadius.circular(10)
+             ),
+             child: ListTile(
+               title:const Text("Call Max"),
+               subtitle:const Text("20:15 april 29",style: TextStyle(color: Colors.red,
+               fontSize: 18
+               ),),
+               trailing: Checkbox(value: true, onChanged:(value) {},),
+             ),
+           ),
+
+         const  SizedBox(height: 10,),
+            const  Text("today",style: TextStyle(fontSize: 17),),
+            CheckBoxTodo(size: size),
+            CheckBoxTodo(size: size),
+          
+       ],
+      ),
+    ),
+    );
+  }
+}
+
+class CheckBoxTodo extends StatelessWidget {
+  const CheckBoxTodo({
+    Key? key,
+    required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+     width: double.infinity,
+     height: size.height/12.2,
+       decoration: BoxDecoration(
+      
+       borderRadius: BorderRadius.circular(10)
+     ),
+     child: ListTile(
+       title:const Text("Call Max"),
+       subtitle:const Text("19:15 ",style: TextStyle(color: Color.fromARGB(255, 82, 79, 78),
+       fontSize: 18
+       ),),
+       trailing: Checkbox(value: true, onChanged:(value) {},),
+     ),
+           );
   }
 }
