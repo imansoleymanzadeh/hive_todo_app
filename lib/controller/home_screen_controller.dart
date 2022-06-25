c72745946ef3f224e48376e8097acee9a2c8ec5c
Abolfazl-MI <abolfazlmashhadi837@gmail.com>
2022-06-25 12:11:34 -0400

    databse base functions add and detail view ui Improved

--- lib/controller/home_screen_controller.dart
+++ lib/controller/home_screen_controller.dart
@@ -1,21 +1,36 @@
 import 'dart:developer';
 
-import 'package:get/state_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:hive_todo_app/models/project_model.dart';
 import 'package:hive_todo_app/models/services/hive_services.dart';
 
 class HomeController extends GetxController {
   late HiveServices _dataBaseService;
-
+  List<Project> allProjects = [];
   createProject({required Project project}) async {
     await _dataBaseService.createProject(project: project).then((value) {
       log(value.toString());
     });
   }
 
+  loadAllProjectsFromDb() async {
+    await _dataBaseService.loadAllProject().then((value) {
+      if (value['resualt'] == CrudStatus.succes) {
+        allProjects = value['data'];
+        update();
+      }
+      if (value['resualt'] == CrudStatus.fail) {
+        Get.defaultDialog(title: 'Error!', content: Text(value['data']));
+      }
+    });
+
+  }
+
   @override
   void onInit() {
     _dataBaseService = HiveServices.instance;
+    // loadAllProjectsFromDb();
     super.onInit();
   }
 }
--- lib/models/services/hive_services.dart
+++ lib/models/services/hive_services.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_catch_clause, empty_catches
+
 import 'dart:developer';
 
 import 'package:hive_flutter/hive_flutter.dart';
@@ -21,13 +23,13 @@ class HiveServices {
   initAdaptors() {
     log('*********REGESTRING ADAPTORS*********', name: 'HIVE HELPER');
     Hive.isAdapterRegistered(0)
-        ? log('projectAdaptor was Initalized')
+        ? log('projectAdaptor was Initalized', name: 'HIVE HELPER')
         : Hive.registerAdapter(ProjectAdapter());
     Hive.isAdapterRegistered(1)
-        ? log('projectTask was Initalized')
+        ? log('projectTask was Initalized', name: 'HIVE HELPER')
         : Hive.registerAdapter(ProjectTaskAdapter());
     Hive.isAdapterRegistered(2)
-        ? log('projectStatus was Initalized')
+        ? log('projectStatus was Initalized', name: 'HIVE HELPER')
         : Hive.registerAdapter(StatusAdapter());
   }
 
@@ -47,15 +49,12 @@ class HiveServices {
   Future<Map<String, dynamic>> createProject({required Project project}) async {
     Project? createdProject;
     try {
-      log('*********CREATING PROJECT IN DB*********');
-      await _projectBox!.add(project).then((value) async {
-        log('key is : $value');
-        createdProject = await _projectBox!.getAt(value);
-        log('project name : ${createdProject!.name!}');
-      });
-
+      log('*********CREATING PROJECT IN DB*********', name: 'HIVE HELPER');
+      int addedProjectkeyNumber = await _projectBox!.add(project);
+      createdProject = _projectBox!.getAt(addedProjectkeyNumber);
       return {'resualt': CrudStatus.succes, 'data': createdProject};
     } on HiveError catch (e) {
+      log('********ERROR : ${e.message}*****', name: 'HIVE HELPER');
       return {'resualt': CrudStatus.fail, 'data': e.message};
     } catch (e) {
       return {'resualt': CrudStatus.fail, 'data': e.toString()};
@@ -65,15 +64,62 @@ class HiveServices {
   Future editUpdateProject() async {
     // TODO Implemnt update prject
   }
-  Future delteProject() async {
-    // Todo implemnt the delter project
+  Future<Map<String, dynamic>> deleteSingleProject(
+      {required int projectIndex}) async {
+    try {
+      _projectBox!.deleteAt(projectIndex);
+      return {'resualt': CrudStatus.succes, 'data': true};
+    } on HiveError catch (e) {
+      return {'resualt': CrudStatus.fail, 'data': e.message};
+    } catch (e) {
+      return {'resualt': CrudStatus.fail, 'data': e.toString()};
+    }
   }
 
-  Future loadAllProject() async {
-    // TODO implement the loadAll projects
+  Future<Map<String, dynamic>> clearDb() async {
+    try {
+      _projectBox!.deleteAll(_projectBox!.keys);
+      return {'resualt': CrudStatus.succes, 'data': true};
+    } on HiveError catch (e) {
+      return {'resualt': CrudStatus.fail, 'data': e.message};
+    } catch (e) {
+      return {'resualt': CrudStatus.fail, 'data': e.toString()};
+    }
   }
 
-  Future loadSingleProject() async {
-    //TODO implemnt the singl load project
+  Future<Map<String, dynamic>> loadAllProject() async {
+    try {
+      log('******** GETING ALL PROJECT FROM DB *****', name: 'HIVE HELPER');
+      log(_projectBox!.keys.toString());
+      List<Project> projectList = _projectBox!.values.toList();
+      return {'resualt': CrudStatus.succes, 'data': projectList};
+    } on HiveError catch (e) {
+      log('********ERROR : ${e.message}*****', name: 'HIVE HELPER');
+      return {'resualt': CrudStatus.fail, 'data': e.message};
+    } catch (e) {
+      // throw e;
+      return {
+        'resualt': CrudStatus.fail,
+        'data': e.toString(),
+      };
+    }
+  }
+
+  Future<Map<String, dynamic>> loadSingleProject(
+      {required int projectIndex}) async {
+    try {
+      Project retrivedProject = _projectBox!.getAt(projectIndex)!;
+      return {'resualt': CrudStatus.succes, 'data': retrivedProject};
+    } on HiveError catch (e) {
+      return {'resualt': CrudStatus.fail, 'data': e.message};
+    } catch (e) {
+      return {'resualt': CrudStatus.fail, 'data': e.toString()};
+    }
   }
 }
+
+
+
+// TODO EDIT SINGLE TASK IN PROJECT 
+// TODO DELTE SINGLE TASK IN PROJECT
+// TODO EDIT PROJECT
